5c7fbd90b07499255113e2c9c1f2d1e589cec0a6
Zero-PointEnergy <kuhakushiro@qq.com>
2022-03-25 18:09:40 +0800

    add some code

--- Work_VI(MusicPlayer)/pages/index/index.js
+++ Work_VI(MusicPlayer)/pages/index/index.js
@@ -1,2 +1,13 @@
 // index.js
-Page({})
+Page({
+    data: {
+        currentTab: 0,
+        searchIcon: "images/search.png"
+    },
+    switchTabs(res) {
+        this.setData({
+            currentTab: res.target.dataset.item
+        })
+        console.log("current tab is :", this.data.currentTab);
+    },
+})
